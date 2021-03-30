a40d622fe0fbf265276aeaccc0e81e05c64c6b7b
jnguyen1098 <jnguyen1098@gmail.com>
2021-03-30 18:10:12 -0400

    create functional version of get_state called suppose_state

--- stately.h
+++ stately.h
@@ -12,14 +12,21 @@ struct state_machine {
     int state_table[NUM_STATE][ALPHABET_SIZE + 1];
 };
 
+// TODO turn these into defines LUL
+
 int get_state(struct state_machine *machine)
 {
     return machine->state;
 }
 
+int suppose_state(struct state_machine *machine, int state, const void *input)
+{
+    return machine->state_table[machine->state][machine->map(input)];
+}
+
 int get_next_state(struct state_machine *machine, const void *input)
 {
-    machine->state = machine->state_table[machine->state][machine->map(input)];
+    machine->state = suppose_state(machine, machine->state, input);
     return get_state(machine);
 }
 
