c7e436e34237f8b5d387127518cf0c2c0950475a
jnguyen1098 <jnguyen1098@gmail.com>
2021-03-31 03:23:02 -0400

    valid number example

---
+++ examples/valid_number.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#include "stately.h"
+
+enum input { INVALID, DIGIT, SCIENTIFIC_E, PLUS_MINUS, PERIOD };
+
+int map_chr(const void *chr) {
+    char c = *(char *)chr;
+    if (c >= '0' && c <= '9')
+        return DIGIT;
+    if (c == 'E' || c == 'e')
+        return SCIENTIFIC_E;
+    if (c == '+' || c == '-')
+        return PLUS_MINUS;
+    if (c == '.')
+        return PERIOD;
+    return INVALID;
+}
+
+int main(void)
+{
+   /***************************************
+    *  DFA that accepts either the empty  *
+    *  string, or any sequence of 1s.     *
+    *                                     *
+    *       1                    0,1      *
+    *    +-----+               +----+     *
+    *    |     |               |    |     *
+    *    |    \ /              |   \ /    *
+    * +--+---------+       +---+--------+ *
+    * |            |       |            | *
+    * | Accepting  |       |  Rejecting | *
+    * |            |       |            | *
+    * +-----+------+       +------------+ *
+    *       |                    / \      *
+    *       |                     |       *
+    *       |                     |       *
+    *       +---------------------+       *
+    *                 0                   *
+    **************************************/
+
+    struct state_machine machine = {
+       
+        // Start state
+        .curr_state = 1,
+
+        // Input mapper
+        .map = map_chr,
+
+        // States
+        .state_table = {
+
+            // TRAP STATE
+            [0] = {},
+
+            [1] = {
+                [PLUS_MINUS] = 2,
+                [DIGIT]      = 3,
+                [PERIOD]     = 4,
+            },
+
+            [2] = {
+                [DIGIT]  = 3,
+                [PERIOD] = 4,
+            },
+
+            [3] = {
+                [DIGIT]        = 3,
+                [PERIOD]       = 5,
+                [SCIENTIFIC_E] = 6,
+            },
+
+            [4] = {
+                [DIGIT] = 5,
+            },
+
+            [5] = {
+                [DIGIT]        = 5,
+                [SCIENTIFIC_E] = 6,
+            },
+
+            [6] = {
+                [PLUS_MINUS] = 7,
+                [DIGIT]      = 8,
+            },
+
+            [7] = {
+                [DIGIT] = 8,
+            },
+
+            [8] = {
+                [DIGIT] = 8,
+            },
+
+        }
+
+    };
+
+    struct test_case {
+        char input[16];
+        int expected_result;
+    };
+
+    struct test_case tests[] = {
+        { "1234567", 3 },
+        { "EeEeEeEeE", 0 },
+        { "45345e4", 8 },
+        { "+3.", 5 },
+        { "3e+", 7 },
+        { "0089", 3 },
+        { "-0.1", 5 },
+        { "+3.14", 5 },
+        { "-.9", 5 },
+        { "-90E3", 8 },
+        { "-123.456e789", 8 },
+
+    };
+
+    for (int i = 0; i < (int)(sizeof(tests) / sizeof(*tests)); i++) {
+        printf("Testing case '%s'\n", tests[i].input);
+        SET_STATE(machine, 1);
+        for (int c = 0; tests[i].input[c]; c++) {
+            (void)GET_NEXT_STATE(machine, &tests[i].input[c]);
+        }
+        //assert(GET_STATE(machine) == tests[i].expected_result);
+        if (GET_STATE(machine) != tests[i].expected_result) {
+            printf("Expected %d but got %d\n", tests[i].expected_result, GET_STATE(machine));
+        }
+    }
+
+    puts("Complete");
+
+    return 0;
+}
